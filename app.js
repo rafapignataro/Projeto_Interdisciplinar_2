89ab1350d2d290c01508210ac157a9d6feaeb1fc
Rafael <rafapignataro@gmail.com>
2019-03-25 15:02:11 -0300

    v1.0.0

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const upload = require("express-fileupload");
+const fs = require("fs");
+
 
 const app = express();
 const server = require('http').createServer(app);
 
 app.use('/public', express.static('public'));
+app.use(upload());
 
 //View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -16,4 +21,5 @@ app.use("/", require("./routes/home"));
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
     var addr = server.address();
     console.log("Conectado em ", addr.address + ":" + addr.port);
+
 });
