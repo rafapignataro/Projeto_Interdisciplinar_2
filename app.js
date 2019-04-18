633f537463666958c179cbe11c9d8686a3185ee3
Rafael Pignataro <rafapignataro@gmail.com>
2019-04-18 16:07:25 -0300

    blabla

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ const http = require('http');
 const upload = require("express-fileupload");
 const fs = require("fs");
 
-
 const app = express();
 const server = require('http').createServer(app);
-
-app.use('/public', express.static('public'));
-app.use(upload());
 
 //View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //Endereçamento de portas
-app.use("/", require("./routes/home"));
+app.use("/", require("./routes"));
+
+app.use('/public', express.static('public'));
+app.use(upload());
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
     var addr = server.address();
